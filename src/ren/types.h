9fc95849528bc521d666993b2182512ad0a49610
Nick Wanninger <nickwanninger@gmail.com>
2025-07-06 21:50:17 -0500

    update types.h glm includes

--- src/ren/types.h
+++ src/ren/types.h
@@ -16,12 +16,17 @@
 
 #include <fmt/core.h>
 
-#define GLM_FORCE_RADIANS
+// #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_ENABLE_EXPERIMENTAL
 
 #include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+
 
 #include <json/json.hpp>
 using json = nlohmann::json;
