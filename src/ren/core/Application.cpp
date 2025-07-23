e4d1b52df027c45e73bb24d723938f392fd54014
Nick Wanninger <nickwanninger@gmail.com>
2025-07-23 11:27:52 -0400

    application: transition to PSOs and abstract work

--- src/ren/core/Application.cpp
+++ src/ren/core/Application.cpp
@@ -103,31 +103,6 @@ namespace ren {
     ren::PipelineCache pipelineCache;
     auto &vulkan = ren::getVulkan();
 
-    ren::DescriptorLayoutCache descriptorLayoutCache;
-
-
-    // ---------------------------------------------------
-
-    // Okay. lets make a simple g buffer opaque pass.
-    RenderPass::Description renderPassDesc;
-    renderPassDesc.name = "GBuffer Pass";
-    // Add a few attachments to the render pass for the gbuffer.
-
-    // HDR emissive data.
-    renderPassDesc.addColorAttachment("emissive", VK_FORMAT_R8G8B8A8_UNORM);
-    // Albedo/diffuse color data.
-    renderPassDesc.addColorAttachment("albedo", VK_FORMAT_R8G8B8A8_UNORM);
-    // World space normal data.
-    renderPassDesc.addColorAttachment("normal", VK_FORMAT_R16G16B16A16_SNORM);
-    // PBR data (R= metallic, G=roughness, B=?).
-    renderPassDesc.addColorAttachment("pbr", VK_FORMAT_R8G8B8A8_UNORM);
-    renderPassDesc.addDepthAttachment("depthStencil");
-
-
-    auto renderPass = renderer->getRenderPassCache().get(renderPassDesc);
-
-
-
 
     int pixelScale = 3;
     float fov = 90.0f;
@@ -140,62 +115,17 @@ namespace ren {
 
 
 
-
-    DescriptorLayoutInfo gbufferLayoutInfo;
-    auto gbufferLayout = descriptorLayoutCache.createLayout(gbufferLayoutInfo);
-    {
-      REN_PROFILE_SCOPE("Create GBuffer Descriptor Set Layout");
-
-      VkDescriptorSetLayoutCreateInfo layoutInfo{};
-      layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-      layoutInfo.bindingCount = 0;
-      layoutInfo.pBindings = nullptr;
-
-      if (vkCreateDescriptorSetLayout(vulkan.device, &layoutInfo, nullptr, &gbufferLayout) !=
-          VK_SUCCESS) {
-        throw std::runtime_error("failed to create descriptor set layout!");
-      }
-    }
-
-
-    // but, to make those buffers, we need to make a render pipeline!
-    // auto gbufferPipeline =
-    //     StandardPipeline(renderPass, makeRef<VertexShader>("shaders/opaque.vert.spv"),
-    //                      makeRef<FragmentShader>("shaders/opaque.frag.spv"), gbufferLayout);
-    ren::PipelineStateObject opaquePSO;
-    opaquePSO.debugName = "GBuffer PSO";
-    opaquePSO.program = makeRef<ShaderProgram>("shaders/opaque");
-    // opaquePSO.blendMode = ren::BlendMode::Additive;
-
     ren::PipelineStateObject blitPSO;
     blitPSO.debugName = "GBuffer Blit PSO";
     blitPSO.program = makeRef<ShaderProgram>("shaders/display");
     blitPSO.cullMode = ren::CullMode::None;
+    blitPSO.hasVertexBinding = false;
 
 
 
     float modelScale = 1.0f;
     glm::vec3 modelRotation(0.0f);
     glm::vec3 modelPosition(0.0f);
-
-
-    // Let's make a sampler for the gbuffers
-    ren::Sampler gbufferSampler(VK_FILTER_NEAREST);
-
-    std::vector<VkDescriptorSet> imguiTextures;
-
-    auto refreshImGuiTextures = [&]() {
-      // Update the ImGui textures with the new gbuffer textures.
-      imguiTextures.clear();
-      for (auto &attachment : gbufferTarget->getAttachments()) {
-        imguiTextures.push_back(ImGui_ImplVulkan_AddTexture(
-            gbufferSampler.getHandle(), attachment.texture->getImageView(),
-            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL));
-      }
-    };
-
-
-
 
 
     while (this->running) {
@@ -211,6 +141,8 @@ namespace ren {
           std::chrono::duration<float, std::chrono::seconds::period>(currentTime - lastTime)
               .count();
       lastTime = currentTime;
+
+
 
       {
         bool windowResized = false;
@@ -231,6 +163,9 @@ namespace ren {
         REN_PROFILE_COUNTER("SDL Events", eventsHandled);
       }
 
+
+
+
       if (!running) break;
 
       // Get a frame from the swapchain.
@@ -240,18 +175,12 @@ namespace ren {
 
       auto frameStats = frame.perf.nextFrame(frame.commandBuffer);
 
-
-
       REN_PROFILE_COUNTER("Memory Usage MB", ren::getCurrentProcessRSS() / (1024.0 * 1024.0));
 
 
 
 
       auto gbufferTarget = sceneRenderer.render(sceneLayer->scene, sceneLayer->camera);
-      if (gbufferTarget) {
-        // Transition the gbuffer target to shader read-only.
-        gbufferTarget->transitionToShaderReadonly(frame.commandBuffer);
-      }
 
       renderer->withPass(*renderer->getDisplayPass(), *frame.renderTarget, [&]() {
         auto cmd = ren::getFrameData().commandBuffer;
@@ -261,6 +190,7 @@ namespace ren {
 
           frame.perf.begin(cmd, "Blit GBuffer");
           renderer->bind(blitPSO);
+
           // begin binding set zero, which is the gbuffer textures.
           auto textureBinder = renderer->startBinding(0);
           auto &attachments = gbufferTarget->getAttachments();
@@ -288,66 +218,41 @@ namespace ren {
 
           int windowWidth, windowHeight;
           SDL_GetWindowSize(getWindow(), &windowWidth, &windowHeight);
-          // float windowWidth = (float)ImGui::GetWindowWidth();
-          // float windowHeight = (float)ImGui::GetWindowHeight();
-
           ImGuizmo::SetRect(0.0f, 0.0f, windowWidth, windowHeight);
 
 
           // Before rendering, lets create a dockspace
           ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport(),
                                        ImGuiDockNodeFlags_PassthruCentralNode);
+
+          ImGui::ShowMetricsWindow();
         }
+
+
+        ImGui::Begin("Gbuffer image pointers");
+        ImGui::Text("GBuffer Target: %s", gbufferTarget ? "Valid" : "Invalid");
+
+        auto displayRendertarget = [&](auto &rt, const char *name) {
+          if (rt) {
+            ImGui::Text("%s Target Size: %ux%u", name, rt->getWidth(), rt->getHeight());
+            for (auto &attachment : rt->getAttachments()) {
+              ImGui::Text("Attachment %s: %p, %p", attachment.name.c_str(),
+                          attachment.texture->getImage(), attachment.texture->getImageView());
+            }
+            ImGui::Separator();
+          }
+        };
+
+        displayRendertarget(gbufferTarget, "GBuffer");
+        displayRendertarget(frame.renderTarget, "Display");
+
+
+        ImGui::End();
+
 
         ImGui::Begin("Asset Manager");
         getAssetManager().inspect();
         ImGui::End();
-
-
-        ImGui::Begin("G-Buffer Viewer");
-
-
-        ImGui::Text("vulkan references: %zu", getVulkanRef().use_count());
-
-        ImGui::DragFloat3("Position", &modelPosition.x, 0.01f);
-        ImGui::DragFloat3("Rotation", &modelRotation.x, 0.01f);
-        ImGui::DragFloat("Scale", &modelScale, 0.01f);
-        ImGui::DragFloat("FOV", &fov, 0.1f, 30.0f, 360.0f);
-
-
-        if (ImGui::SliderInt("Pixel Scale", &pixelScale, 1, 10)) { targetValid = false; }
-
-        ImGui::Separator();
-
-        for (auto &[name, value] : frameStats) {
-          ImGui::Text("%15s: %10.5fms", name.c_str(), value);
-        }
-
-        ImGui::Separator();
-
-
-        auto attachments = gbufferTarget->getAttachments();
-        for (int i = 0; i < attachments.size(); i++) {
-          // if (i != 2) continue;
-          auto &attachment = attachments[i];
-          ImGui::Text("Attachment: %s", attachment.name.c_str());
-          float width = ImGui::GetContentRegionAvail().x;
-          float height = width / renderAspect;
-          // ImGui::Image((ImTextureID)imguiTextures[i], ImVec2(width, height));
-          ImGui::Separator();
-        }
-        ImGui::End();
-
-        ImGui::Begin("PSO");
-        ImGui::Text("Pipeline cache size: %zu", renderer->getPipelineCache().size());
-        json j = opaquePSO;
-        ImGui::Text("Pipeline State Object: %s", j.dump(2).c_str());
-        // hash of that object:
-        ImGui::Text("Hash: %llu", ren::hash(opaquePSO));
-        opaquePSO.renderInspector();
-        ImGui::End();
-
-        // graph.renderImGui();
 
         layerStack.onImGuiRender(deltaTime);
 
