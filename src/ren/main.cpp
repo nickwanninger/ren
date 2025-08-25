6c5617f37893a9078420d7e3459505dad18ece6f
Nick Wanninger <nickwanninger@gmail.com>
2025-08-24 23:36:32 -0500

    Add a simple ECS scene to main.cpp

--- src/ren/main.cpp
+++ src/ren/main.cpp
@@ -8,15 +8,57 @@
 #include <flecs.h>
 
 #include <type_traits>
+#include <imgui.h>
+#include <ren/core/AutoPlugin.h>
+
+#include <imgui.h>
+
+struct CubeWave {};
+
+static void test_plugin(ren::Application &app) {
+  app.world.component<CubeWave>();
+
+  app.world.system<ren::comp::Transform>("ren.core.TestSystem")
+      .with<CubeWave>()
+      .multi_threaded(true)
+      .each([](ren::comp::Transform &t) {
+        // REN_PROFILE_SCOPE("Test");
+        // t.scale = glm::vec3(0.2f);
+        t.translation.y =
+            sin(t.translation.x / 8.0f + t.translation.z / 8.0f + ren::timeSeconds() * 2.0f) * 2.0f;
+      });
+}
+
+REN_PLUGIN("Test Plugin", test_plugin);
 
 
 
 int main(int argc, char *argv[]) {
- REN_PROFILE_BEGIN_SESSION("Engine Run", "engine_run_profile.json");
-  ren::Application app("ren", {2560, 1440});
+  try {
+    ren::Application app("ren", {2560, 1440});
 
-  app.run();
+    auto cube = ren::MeshScene::load("assets/test/meshes/unit_cube.glb");
+    int scale = 50;
+    auto root = app.sceneLayer->scene.createEntity("root");
+    for (int x = 0; x < scale; x++) {
+      for (int z = 0; z < scale; z++) {
+        auto e = app.sceneLayer->scene.createEntity().child_of(root).add<CubeWave>();
 
-  REN_PROFILE_END_SESSION();
+        ren::comp::Transform transform;
+        transform.translation.x = (x - scale / 2) * 2.0f;
+        transform.translation.y = 0;
+        transform.translation.z = (z - scale / 2) * 2.0f;
+        transform.scale = glm::vec3(0.75f);
+        e.set<ren::comp::Transform>(transform);
+
+        cube->instantiate(e);
+      }
+    }
+
+    app.world.entity<ren::RenderDebug>();
+
+    app.run();
+  } catch (const std::exception &e) { fmt::print("Error: {}\n", e.what()); }
+
   return 0;
 }
