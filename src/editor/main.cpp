c3740d3833dc8e8d2ac5658e84563fae88a3a186
Nick Wanninger <nickwanninger@gmail.com>
2025-11-04 23:13:48 -0600

    Cleanup the main function in the editor (todo: make it an editor :))

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -34,35 +34,16 @@ int main(int argc, char *argv[]) {
 
 
 
+
   ren::Application app("Editor", res);
 
-  // loadMeshIntoScene("/Users/nick/Downloads/tiny_isometric_room.glb", 0.01f);
-  // loadMeshIntoScene("/Users/nick/Downloads/DamagedHelmet.glb", 1.0f);
-  // loadMeshIntoScene("/Users/nick/Downloads/MetalRoughSpheres.glb", 1.0f);
-  // loadMeshIntoScene("/Users/nick/Downloads/neighbourhood_city_modular_lowpoly.glb", 1.0f);
-  // loadMeshIntoScene("/Users/nick/Downloads/NormalTangentTest.glb", 1.0f);
-  // loadMeshIntoScene("/Users/nick/Downloads/NormalTangentMirrorTest.glb", 1.0f);
-
-  // loadMeshIntoScene("/Users/nick/Desktop/sponza.glb");
-  // loadMeshIntoScene("/Users/nick/Desktop/RamenCup.glb");
-  loadMeshIntoScene("assets/test/meshes/simple_scene.glb");
-  // loadMeshIntoScene("assets/test/meshes/unit_cube.glb");
-  // loadMeshIntoScene("/Users/nick/dev/kajiya/assets/meshes/viziers_observation_deck/scene.gltf",
-  // 0.01);
-
-  // loadMeshIntoScene(
-  //     "/Users/nick/dev/kajiya/assets/meshes/flying_world_-_battle_of_the_trash_god/scene.gltf",
-  //     0.002f);
-  // loadMeshIntoScene("/Users/nick/Desktop/enrico.glb");
-  // loadMeshIntoScene("/Users/nick/Downloads/main_sponza/NewSponza_Main_glTF_003.gltf");
-  // loadMeshIntoScene("/Users/nick/Downloads/pkg_a_curtains/NewSponza_Curtains_glTF.gltf");
-  // loadMeshIntoScene("/Users/nick/Downloads/pkg_b_ivy/NewSponza_IvyGrowth_glTF.gltf");
-  // loadMeshIntoScene("/Users/nick/Downloads/pkg_c_trees/NewSponza_CypressTree_glTF.gltf");
-
-  // loadMeshIntoScene("/Users/nick/Downloads/huge_icelandic_lava_cliff_sieoz_high.glb");
-  // loadMeshIntoScene("/Users/nick/Downloads/Rock.glb");
-  // loadMeshIntoScene("/Users/nick/Downloads/broken_wall_slunl_high.glb");
-  // loadMeshIntoScene("/Users/nick/Downloads/broken_stump_rkswd_raw.glb");
+  if (argc == 1) {
+    loadMeshIntoScene("assets/test/meshes/simple_scene.glb");
+  } else {
+    for (int i = 1; i < argc; ++i) {
+      loadMeshIntoScene(argv[i]);
+    }
+  }
 
   app.run();
 
