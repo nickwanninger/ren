6450a7d098bf70729e0dab7c8480cf91f239b228
Nick Wanninger <nickwanninger@gmail.com>
2025-12-29 11:00:15 -0600

    remove the capture dir from the renderdoc script

--- tools/renderdoc.sh
+++ tools/renderdoc.sh
@@ -6,6 +6,4 @@ export DISPLAY=:0
 export XAUTHORITY=/run/user/1000/xauth_PnibNW
 
 
-CAPTUREDIR=./rdoc
-mkdir -p ${CAPTUREDIR}
-renderdoccmd capture -w -d . -c ${CAPTUREDIR} $@
+renderdoccmd capture -w -d . $@
